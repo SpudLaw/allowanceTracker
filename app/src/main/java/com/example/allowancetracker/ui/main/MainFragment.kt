1aee43f0fed03cb92682ed579f9267c9500edd4f
Caroline <carolinealaw@gmail.com>
2021-11-13 08:35:36 -0800

    add list item to listview

--- app/src/main/java/com/example/allowancetracker/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/allowancetracker/ui/main/MainFragment.kt
@@ -1,9 +1,11 @@
 package com.example.allowancetracker.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -23,19 +25,31 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        binding.allowanceValue.text = "$" + viewModel.allowance.toString()
+
         binding.addPurchaseButton.setOnClickListener {
             with(binding.addPurchaseDialog) {
+
+                costEditText.text?.clear()
+                descriptionEditText.text?.clear()
+
                 dialogPositiveButton.setOnClickListener {
                     toggleDialogVisibility(false)
 
-                    val cost: Double = costEditText.text.toString().toDouble()
-                    val description: String = descriptionEditText.text.toString()
-                    val purchase: Purchase = Purchase(cost, null, description)
+                    if (costEditText.text?.isNotBlank() == true && descriptionEditText.text?.isNotBlank() == true) {
+                        val cost: Double = costEditText.text?.toString()?.toDouble() ?: 0.0
+                        val description: String = descriptionEditText.text.toString()
+                        val purchase = Purchase(cost, null, description)
 
-                    viewModel.add(purchase)
+                        viewModel.add(purchase)
+                    } else {
+                        Toast.makeText(context, "Please enter cost and description", Toast.LENGTH_LONG).show()
+                    }
                 }
 
                 dialogNegativeButton.setOnClickListener {
--- app/src/main/java/com/example/allowancetracker/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/allowancetracker/ui/main/MainViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+    var allowance: Double = 400.0
     var purchases: MutableList<Purchase> = mutableListOf()
 
     fun add(purchase: Purchase) {
